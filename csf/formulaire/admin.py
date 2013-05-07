efc78b4ba696ee69919a85f67589ba4b94f787fd
Benoit C. Sirois <benoit.clennett-sirois@auf.org>
2013-05-07 15:07:38 -0400

    Ajout de filter pour admin

--- csf/formulaire/admin.py
+++ csf/formulaire/admin.py
@@ -1,8 +1,11 @@
+# -*- encoding: utf-8 -*-
+
 from django.contrib import admin
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
 from django.contrib.auth.models import User
+from django.contrib.admin import SimpleListFilter
 from auf.django.auth_token.admin import TokenUserAdmin
 from django.core.urlresolvers import reverse
 from .models import (
@@ -49,10 +52,39 @@ def show_link(obj):
 show_link.short_description = _(u'Lien')
 show_link.allow_tags = True
 
+
+class IsEtablissementFilter(SimpleListFilter):
+    title = 'type'
+    parameter_name = 'is_etab'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Etablissement'),
+            ('no', 'Utilisateur'),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == 'no':
+            return queryset.filter(
+                etablissement_eligible__id__isnull=True)
+
+        if self.value() == 'yes':
+            return queryset.exclude(
+                etablissement_eligible__id__isnull=True)
+
+
 class UserAdmin(TokenUserAdmin):
 
     list_display = TokenUserAdmin.list_display + [show_link]
     inlines = TokenUserAdmin.inlines + [EtablissementEligibleInline]
+    list_filter = list(TokenUserAdmin.list_filter) + [IsEtablissementFilter]
 
 
 class OrderedAdmin(admin.ModelAdmin):

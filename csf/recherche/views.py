32d21b68b187121b90e3022af73fa150889d7b3d
root <caravan.coop>
2013-09-06 01:02:46 +0000

    multiple correction de l'affichage et de la recheche

--- csf/recherche/views.py
+++ csf/recherche/views.py
@@ -20,6 +20,17 @@ class SearchView(TemplateView):
             
         return context
 
+
+def _check_niveaux(new_object_list, obj):
+    for new_obj in new_object_list:
+        if new_obj['discipline'] == obj.discipline and \
+           new_obj['pk'] == obj.etablissement.etablissement.pk:
+
+            new_obj['niveaux'].append(obj.niveau)
+            return False
+
+    return True
+
 class EtabliListView(ListView):
     model = OffreFormation
 
@@ -30,39 +41,54 @@ class EtabliListView(ListView):
         context = super(EtabliListView, self).get_context_data(**kwargs)
 
         context['form'] = SearchForm()
-        context['filter'] = OffreFormationFilter(self.request.GET, queryset=OffreFormation.objects.all())
+        context['filter'] = context['object_list']
 
+        new_object_list = []
+        for obj in context['object_list'].qs:
 
-        new_object_list = {}
-        for obj in context['object_list']:
-            etabli = obj.etablissement.etablissement
-            if obj.discipline in new_object_list.keys():
-                new_object_list[obj.discipline].append({etabli.nom: {'object': obj, 'niveaux':[obj.niveau]}})
-            else:
-                new_object_list[obj.discipline] = [{etabli.nom: {'object': obj, 'niveaux':[obj.niveau]}}]
+            need_to_add = _check_niveaux(new_object_list, obj)        
+            if need_to_add:
+                new_object_list.append(
+                    {
+                     'discipline': obj.discipline, 
+                      'pk': obj.etablissement.etablissement.pk,
+                      'nom': obj.etablissement.etablissement.nom,
+                      'pays': obj.etablissement.etablissement.pays.nom,
+                      'niveaux': [obj.niveau],
+                      }
+                 )
 
         context['object_list'] = new_object_list
 
         return context
 
     def get_queryset(self):
-        return OffreFormationFilter(self.request.GET, queryset=OffreFormation.objects.all())
+        return OffreFormationFilter(self.request.GET, queryset=OffreFormation.objects.all()\
+                  .select_related('niveau', 'discipline', 'etablissement__etablissement',
+                                  'etablissement__etablissement__pays'))
+
 
 class EtabliDetailView(DetailView):
     model = OffreFormation
     template_name = "formulaire/etabli.html"
 
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get(self.pk_url_kwarg, None)
+        return OffreFormation.objects.filter(etablissement__etablissement__pk=pk)[0].etablissement.etablissement
+
     def get_context_data(self, **kwargs):
         context = super(EtabliDetailView, self).get_context_data(**kwargs)
+
         context['form'] = SearchForm()
         context['filter'] = OffreFormationFilter(self.request.GET, queryset=OffreFormation.objects.all())
 
-        context['pays_list'] = \
-            set(OffreFormation.objects.values_list('etablissement__etablissement__pays__code',
-                  'etablissement__etablissement__pays__nom').distinct())
+        context['disciplines'] = [{'discipline': c.discipline, 'niveau': c.niveau} for c in OffreFormation.objects.filter(etablissement__etablissement__pk=context['object'].pk)]
+
         context['object'].dans_le_pays = \
-              OffreFormation.objects.filter(etablissement__etablissement__pays=\
-                                            context['object'].etablissement.etablissement.pays)\
-                .values_list('etablissement__etablissement__pk', 'etablissement__etablissement__nom')
+            set(OffreFormation.objects.filter(etablissement__etablissement__pays__code=\
+                                             context['object'].pays.code)\
+                .values_list('etablissement__etablissement__pk', 'etablissement__etablissement__nom'))
+
+
 
         return context

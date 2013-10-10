0214c6664215690928f3d0fe309e759b8f5c750b
Olivier Larchevêque <olivier.larcheveque@auf.org>
2013-10-10 09:58:36 -0400

    log

--- csf/settings/10-logging.py
+++ csf/settings/10-logging.py
@@ -3,39 +3,20 @@ LOGGING = {
     'disable_existing_loggers': True,
     'root': {
         'level': 'WARNING',
-        'handlers': ['sentry'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
+        'handlers': [
+            # si sentry ne résoud pas sentry.ca.auf.org
+            #'mail_admins',
+            'sentry',
+            ],
     },
     'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
         'sentry': {
             'level': 'WARNING',
             'class': 'raven.contrib.django.handlers.SentryHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
         },
     },
 }
